5a2642c741e77c6badfd620cd491c3e109aeaa7e
Brittany Heller <brittany.heller@live.com>
2019-10-05 11:14:30 -0700

    initial commit

---
+++ CompostDataApi/Controllers/CompostDataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompostDataApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompostDataApi.Controllers
+{
+    [Route("api/compost")]
+    [ApiController]
+    public class CompostDataController : ControllerBase
+    {
+        // GET api/compost/guid
+        [HttpGet("{id}")]
+        public ActionResult<Project> Get(Guid id)
+        {
+            return new Project();
+        }
+
+        // POST api/compost
+        [HttpPost]
+        public void Post()
+        {
+        }
+    }
+}
---
+++ CompostDataApi/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompostDataApi.Models
+{
+    public class Project
+    {
+        public Project(Guid id, DateTime startDate)
+        {
+            Id = id;
+            StartDate = startDate;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
